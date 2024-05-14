706606c696dacec60f39cf254d5dd4bdacd0ffad
Brian Campbell <Brian.Campbell@ed.ac.uk>
2024-05-14 16:57:12 +0100

    Coq: support unusual test cases

--- src/sail_coq_backend/pretty_print_coq.ml
+++ src/sail_coq_backend/pretty_print_coq.ml
@@ -3538,6 +3538,8 @@ end = struct
      work. *)
 
   let type_enum ctxt env type_map =
+    (* A model with a single type of registers is unusual, but handy for testing. *)
+    let is_single = match type_map with [_] -> true | _ -> false in
     separate hardline
       [
         string "Variant register : Type -> Type :=";
@@ -3562,7 +3564,7 @@ end = struct
             ^^ string "_beq r r'"
           )
           type_map;
-        string "  | _, _ => false";
+        (if is_single then empty else string "  | _, _ => false");
         string "  end.";
         empty;
         string
@@ -3577,7 +3579,7 @@ end = struct
             ^^ string "_beq r r' then Some p else None"
           )
           type_map;
-        string "  | _, _ => fun _ => None";
+        (if is_single then empty else string "  | _, _ => fun _ => None");
         string "  end.";
         empty;
       ]
